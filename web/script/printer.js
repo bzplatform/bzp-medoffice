129fa9b281e32fc5652ce9f3e3af7b886acb2513
vladperl <vladperl@127.0.0.1>
2013-08-23 11:06:56 -0400

    labels and reports

--- web/script/printer.js
+++ web/script/printer.js
@@ -11,12 +11,12 @@ $(document).ready(function(){
 );
 function print(orientation, margin) {
     if (jsPrintSetupInstalled) {
-      if (orientation == "Portrait") {
+      if (orientation === "Portrait") {
          jsPrintSetup.setOption('orientation', jsPrintSetup.kPortraitOrientation);
-      } else if (orientation == "Landscape") {
+      } else if (orientation === "Landscape") {
          jsPrintSetup.setOption('orientation', jsPrintSetup.kLandscapeOrientation);
       }
-      if (margin == undefined) {
+      if (margin === undefined) {
          margin = 0;
       }
       jsPrintSetup.setPaperSizeUnit(jsPrintSetup.kPaperSizeMillimeters);
@@ -26,6 +26,43 @@ function print(orientation, margin) {
       jsPrintSetup.setOption('marginRight', margin);
       jsPrintSetup.clearSilentPrint();
       jsPrintSetup.setOption('printSilent', 1);
+      jsPrintSetup.print();
+   } else {
+      window.print();
+   }
+   return false;
+}
+
+function printLabel(orientation, margin) {
+    if (jsPrintSetupInstalled) {
+      if (orientation === "Portrait") {
+         jsPrintSetup.setOption('orientation', jsPrintSetup.kPortraitOrientation);
+      } else if (orientation === "Landscape") {
+         jsPrintSetup.setOption('orientation', jsPrintSetup.kLandscapeOrientation);
+      }
+      if (margin === undefined) {
+         margin = 0;
+      }
+      jsPrintSetup.setPaperSizeUnit(jsPrintSetup.kPaperSizeMillimeters);
+      jsPrintSetup.setOption('marginTop', margin);
+      jsPrintSetup.setOption('marginBottom', margin);
+      jsPrintSetup.setOption('marginLeft', margin);
+      jsPrintSetup.setOption('marginRight', margin);
+      jsPrintSetup.clearSilentPrint();
+      jsPrintSetup.setOption('printSilent', 1);
+      var printerList = jsPrintSetup.getPrintersList().split(',');
+      var printer = null;
+      for (var i = 0; i < printerList.length; i++) {
+        if (printerList[i].indexOf('LabelWriter') !== -1) {
+            printer = printerList[i];
+            break;
+        }
+      }
+      if (printer === null) {
+         alert('LabelWriter printer has not been installed!');
+         return;
+      }
+      jsPrintSetup.setPrinter(printer);      
       jsPrintSetup.print();
    } else {
       window.print();
